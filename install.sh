dd6094f9215a9170874612010f32901150eccbc9
Erico Lima Schuch <eschuch@gmail.com>
2021-12-06 15:33:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 #
 # Instalador simples.
 # Verifica algumas dependências, instala o que for preciso e inicia o serviço.
+# Para executar, execute a linha abaixo num shell termux:
+# wget https://raw.githubusercontent.com/eschuch/termux-monitor/main/install.sh -O - | bash
 # 
 pkg install wget -y
 pkg install curl -y
@@ -11,11 +13,17 @@ mkdir -p ~/bin
 cd ~/bin
 wget https://codeload.github.com/eschuch/termux-monitor/zip/refs/heads/main -O termux-monitor.zip
 unzip termux-monitor.zip
+mv termux-monitor-main/* ./
+rm -rf termux-monitor-main termux-monitor.zip
 mkdir -p ~/.termux/boot/
 ln -s start-monitor.sh ~/.termux/boot/start-monitor.sh
 chmod +x monitor.sh start-monitor.sh telegram.sh
 #
 #
-nano monitor.sh
-nano telegram.sh
-./start-monitor.sh &
+echo Edite primeiro o arquivo 
+echo nano ~/bin/monitor.sh
+echo Depois o arquivo:
+echo nano ~/bin/telegram.sh
+echo Inicie o servico reiniciando o box ou com o comando
+echo ~/bin/start-monitor.sh &
+echo "################"
