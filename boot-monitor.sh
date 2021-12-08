2b816fe3c0cd8faea6f493695e477ddaea9cebcd
Erico Lima Schuch <eschuch@gmail.com>
2021-12-08 18:20:25 -0300

    Update boot-monitor.sh

--- boot-monitor.sh
+++ boot-monitor.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
+#!/data/data/com.termux/files/usr/bin/sh
 
 # Script simples para iniciar os serviços.
 # espera que além do termux, seja instalado o termux:boot e este escript esteja na pasta:
@@ -9,4 +10,3 @@
 
 termux-wake-lock &
 sshd &
-/data/data/com.termux/files/home/bin/monitor.sh &
