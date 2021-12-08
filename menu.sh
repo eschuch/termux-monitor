bde102b3cf558fbe0a17291d5009e1f7a9feab1a
Erico Lima Schuch <eschuch@gmail.com>
2021-12-08 18:26:06 -0300

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/sh
+DIALOG=${DIALOG=dialog}
+
+$DIALOG --title " My first dialog" --clear \
+        --yesno "Hello , this is my first dialog program" 10 30
+
+case $? in
+  0)
+    echo "Yes chosen.";;
+  1)
+    echo "No chosen.";;
+  255)
+    echo "ESC pressed.";;
+esac
